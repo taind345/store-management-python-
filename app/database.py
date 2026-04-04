ddf1eccfb7edb141b1f62e0e6d2b9f5633ed09e9
Taind <tainguyenduc03042005@gmail.com>
2026-04-04 21:16:44 +0700

    update

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from sqlalchemy.orm import declarative_base
 
 # Thay đổi bằng chuỗi kết nối PostgreSQL thật của bạn
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:T%40i03042005@localhost:5432/sales_db"
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/sales_db"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
